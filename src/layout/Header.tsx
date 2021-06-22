bed8accddd2a68258621b640050804442aa18cd5
maxrogers78 <maxrogerssepulveda@gmail.com>
2021-06-21 20:09:48 -0400

    starter project ready

---
+++ src/layout/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="header container">
+      <a href="#" className="header__logo">
+        AAN
+      </a>
+    </header>
+  );
+};
+
+export default Header;
